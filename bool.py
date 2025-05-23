c0b182a5bfe2d5d271cc1c232f60f965dc6431cf
Gyuhyeok Lee <daniel208847@gmail.com>
2025-05-23 22:15:16 +0100

    bull practice

---
+++ bool.py
@@ -0,0 +1,26 @@
+# bull(number)
+a = (1 == 1)
+b = (1 == 2)
+print(a)
+print(b)
+
+a = (1 < 2)
+b = (1 > 2)
+print(a)
+print(b)
+
+# bull(string)"
+a = ("nihao" == "hello")
+b = ("hello" == "world")
+print(a)
+print(b)
+
+# bull(calculation)
+a = bool([1, 2, 3])
+b = bool([])
+c = bool([0])
+print(a)
+print(b)
+print(c)
+
+
